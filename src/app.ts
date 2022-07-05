597aa05f1b83cee9f4ed1e607edeeffa7cacf227
Gid.Lamim <gid.lamim@gmail.com>
2022-07-05 16:26:58 -0300

    complete Desafio2
    corrigido os erros
    
    adicionado uma templete para a criação dos objetos pessoas e um enum com as listas de profissões

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,45 @@
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
-// quando crio um objeto, eu devo colocar o tipo de variável dentro dele.
-
-//abaixo temos algumas formas de corrigir esses erros.
-
-//caso1 - nesse caso eu apenas criei o objeto com as variáveis. Porém, as variáveis sempre serão do tipo que foram criadas. 
-let employee1 = {
-    code: 11,
-    name: "Peter" 
- };
 
 
-//caso2 - nesse caso eu estou forçando que os tipos de variàveis sejam dos tipos number e string.
-let employee2: {code: number, name: string} = {
-   code: 12,
-   name: "john" 
+// Como podemos melhorar o esse código usando TS? 
+
+ interface pessoa {
+    nome: String,
+    idade: Number,
+    profissao: profissão // a profissão será do tipo enum. isso melhora a padronização
+}
+
+//An enum type is a special data type that enables for a variable to be a set of predefined constants. 
+enum profissão {
+    Atriz,
+    Engenheiro,
+    Pintor,
+    Padeiro
+}
+
+let pessoa1: pessoa  = {//pessoa1 criada como um objeto e implementando a interface Pessoa
+    nome: "maria",
+    idade: 29,
+    profissao: profissão.Atriz
+}
+
+let pessoa2: pessoa = {//pessoa2 criada como um objeto e implementando a interface Pessoa
+    nome: "roberto",
+    idade: 19,
+    profissao: profissão.Padeiro
+}
+
+let pessoa3: pessoa = {//pessoa3 implementando a interface Pessoa
+    nome: "laura",
+    idade: 32, //a idade era uma string. Ao implementar a interface, ficou fácil localizar o erro
+    profissao: profissão.Atriz
 };
 
-//caso3 - utilizando uma interface, nós criamos um contrato. Ao criar um objeto usando a interface, ele será forçado a tipar as variáveis.
-
-interface employee {
-    code: number,
-    name: string
+let pessoa4: pessoa = {//pessoa4 implementando a interface Pessoa
+    nome: "carlos",
+    idade: 19,
+    profissao: profissão.Padeiro
 }
-// ao criar o objeto, eu digo q ele será do tipo employee (interface) e sou obrigado a implementar code e name
-let employee3: employee = {
-    code: 12,
-    name: "Milla"
-}
-
-
-//não temos mais erros
-employee1.code = 10;
-employee1.name = "Peter Jones";
-
-employee2.code = 11;
-employee2.name = "John Bosco";
-
-console.log(employee1.name);
+console.log(`${pessoa1.nome} tem ${pessoa1.idade} e trabalha como ${pessoa1.profissao}`);
+console.log(`${pessoa2.nome} tem ${pessoa2.idade} e trabalha como ${pessoa2.profissao}`);
+console.log(`${pessoa3.nome} tem ${pessoa3.idade} e trabalha como ${pessoa3.profissao}`);
+console.log(`${pessoa4.nome} tem ${pessoa4.idade} e trabalha como ${pessoa4.profissao}`);
--- src/desafios/desafio1.js
+++ src/desafios/desafio1.js
@@ -1,5 +1,30 @@
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+// ****  FEITO ****
 
-let employee = {};
-employee.code = 10;
-employee.name = "John";
+// quando crio um objeto, eu devo colocar o tipo de variável dentro dele.
+
+//abaixo temos algumas formas de corrigir esses erros.
+
+//caso1 - nesse caso eu apenas criei o objeto com as variáveis. Porém, as variáveis sempre serão do tipo que foram criadas. 
+let employee1 = {
+    code: 11,
+    name: "Peter" 
+ };
+
+
+//caso2 - nesse caso eu estou forçando que os tipos de variàveis sejam dos tipos number e string.
+let employee2: {code: number, name: string} = {
+   code: 12,
+   name: "john" 
+};
+
+//caso3 - utilizando uma interface, nós criamos um contrato. Ao criar um objeto usando a interface, ele será forçado a tipar as variáveis.
+
+interface employee {
+    code: number,
+    name: string
+}
+// ao criar o objeto, eu digo q ele será do tipo employee (interface) e sou obrigado a implementar code e name
+let employee3: employee = {
+    code: 12,
+    name: "Milla"
+}
